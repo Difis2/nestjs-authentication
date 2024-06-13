27754b87ff745644cd5bbcf03b97da7791860627
Diogo <diogofgsoares@gmail.com>
2024-06-13 15:53:48 +0100

    test db connection

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { DrizzleAsyncProvider } from '../drizzle/drizzle.provider';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import * as schema from "./../drizzle/schema";
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @Inject(DrizzleAsyncProvider) private db: NodePgDatabase<typeof schema>
+  ) {}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,8 +19,10 @@ export class UserService {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    return await this.db.query.user.findFirst({
+      where: eq(schema.user.id, id),
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
